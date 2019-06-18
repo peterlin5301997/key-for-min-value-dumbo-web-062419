4217bb2cce560c382634a50fe94fd03536519029
enchanting-process-7328 <peterlin5301997@gmail.com>
2019-06-18 22:17:09 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,17 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  if name_hash.size == 0
+    return nil
+  else
+    lowest = 999
+    loser = ""
+    name_hash.collect do |name, num|
+      if num < lowest
+        loser = name
+        lowest = num
+      end
+    end
+    return loser
+  end
 end
