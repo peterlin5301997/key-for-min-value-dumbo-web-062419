b17a7cab3ebb159a8e9c8541d2af4900605f130b
enchanting-process-7328 <peterlin5301997@gmail.com>
2019-06-18 21:55:55 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,9 +2,17 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  name_hash.each do |name|
-    if name < minValue
-      minValue = name
+  if name_hash.size == 0
+    return nil
+  else
+    name_hash.each do |name|
+      if name == name.first
+        minValue = name
+      else
+        if name < minValue
+          minValue = name
+        end
+      end
     end
   end
   return minValue
