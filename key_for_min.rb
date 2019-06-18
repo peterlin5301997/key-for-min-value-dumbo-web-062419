5bd5f8d500cd5ede467f94fd03c5c82c99a973c3
enchanting-process-7328 <peterlin5301997@gmail.com>
2019-06-18 22:05:59 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   if name_hash.size == 0
     return nil
   else
-    lowest = name_hash.first
+    lowest = name_hash[first]
     name_hash.collect do |name, num|
       if num < value1
         x = name
